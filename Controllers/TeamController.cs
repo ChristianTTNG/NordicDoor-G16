3e193a27b2c90f87c577f8bf1e9f4ca951200c1b
Isakkm <isakkm@uia.no>
2022-11-08 11:08:18 +0100

    Make some comments

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -12,37 +12,37 @@ namespace NordicDoors.Controllers
 {
     public class TeamController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;  // Deklarerer en variabel "_context" av typen (klassen) "ApplicationDbContext", som ligger i "Data" mappen vår
 
-        public TeamController(ApplicationDbContext context)
+        public TeamController(ApplicationDbContext context)  // Sender inn parameter "context"
         {
-            _context = context;
+            _context = context;   // sier at variabel / field / attributt "_context"  =  parameteren vår "context" 
         }
 
         // GET: Team
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index() // Index er hovedsiden, eller default, av urlen
         {
-              return _context.Team != null ? 
-                          View(await _context.Team.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Team'  is null.");
+              return _context.Team != null ?       // Her er en conditional operator, som gjører koden under    (  Condition ?   run if true  :  run if false  ) 
+                          View(await _context.Team.ToListAsync()) :      // gjør denne linja "if true ^".  Await gjør at den venter til "_context.Team.ToListAsync()" er ferdig med å hente alle instances i tabellen/entiteten vår "Team" før den sender tilbake view "return View()"
+                          Problem("Entity set 'ApplicationDbContext.Team' is null.");   // Returnerer en respons
         }
 
         // GET: Team/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? id) 
         {
-            if (id == null || _context.Team == null)
+            if (id == null || _context.Team == null)  // hvis parameter "id" eller "_context.Team" er NULL --> kjør kode under
             {
                 return NotFound();
             }
 
-            var team = await _context.Team
+            var team = await _context.Team      // venter på linja under er ferdig (går igjennom liste med "TeamID" til den finner en som er lik "id" parameteret)
                 .FirstOrDefaultAsync(m => m.TeamID == id);
-            if (team == null)
+            if (team == null)       
             {
                 return NotFound();
             }
 
-            return View(team);
+            return View(team);      // returnerer View    (Team/Details/5)
         }
 
         // GET: Team/Create
@@ -56,7 +56,7 @@ namespace NordicDoors.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TeamID,TeamName,TeamSize")] Team team)
+        public async Task<IActionResult> Create([Bind("TeamID,TeamName")] Team team)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace NordicDoors.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TeamID,TeamName,TeamSize")] Team team)
+        public async Task<IActionResult> Edit(int id, [Bind("TeamName")] Team team)
         {
             if (id != team.TeamID)
             {
